85b09f3d129852054c280239a881736a730d2421
Peter <searisetech@gmail.com>
2023-10-31 19:44:37 -0400

    third commit

--- userMgt.java
+++ userMgt.java
@@ -6,4 +6,5 @@ bunme
 Glory and Ope is here learning git for the first time
 Glory is a DevOps Engineer
 Ope is a DevOps Engineer
-
+Prof Yinka is also learning git
+We are a qualify DevOps Engineers
